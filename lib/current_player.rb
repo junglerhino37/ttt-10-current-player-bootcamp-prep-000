5ab1e3284bba428905cf53fea518cea4507309a2
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-03 00:20:54 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,17 +13,8 @@ def  current_player(board)
   turn=turn_count(board)
   puts "totasl turns is #{turn}"
   turn%2==0 ? "X":"O"
-  end
+  
 end
-  turns=turn_count(board)
-  if (turns%2)
-    return "O"
-  else 
-    return "X"
-  end
-end
-
-
 
 board = [" ", "O", " ", " ", "", " ", " ", " ", "X"]
 puts current_player(board)
